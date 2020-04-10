fce748c6c3a563a90a6dd88ac5f0d5a66603918e
hachi <eojin312@naver.com>
2020-04-10 16:47:50 +0900

    hachishop 회원가입 예제 처리

--- src/main/java/hachi/hachishop/Controller/MemberForm.java
+++ src/main/java/hachi/hachishop/Controller/MemberForm.java
@@ -1,7 +1,6 @@
 package hachi.hachishop.Controller;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
@@ -12,9 +11,13 @@ public class MemberForm {
     @NotEmpty(message = "회원이름은 필수 입니다")
     private String name;
 
+    @NotEmpty(message = "도시를 꼭 넣어주세요")
     private String city;
 
+    @NotEmpty(message = "주소를 꼭 넣어주세요")
     private String street;
+
+    @NotEmpty(message = "우편번호는 필수 입니다")
     private String zipcode;
 
 }
