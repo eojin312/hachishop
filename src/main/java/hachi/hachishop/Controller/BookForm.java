fc67a8f23ca5ef44b6ab43a2df3a9a7988c84e1d
hachi <eojin312@naver.com>
2020-04-13 13:50:06 +0900

    hachishop vaild 으로 예외처리 삭제 나중에 좀 더 공부해서 보강

--- src/main/java/hachi/hachishop/Controller/BookForm.java
+++ src/main/java/hachi/hachishop/Controller/BookForm.java
@@ -14,14 +14,15 @@ public class BookForm {
     //상품의 공통속성
     @NotEmpty(message = "상품이름은 필수 입니다")
     private String name;
-    @NotEmpty(message = "상품가격은 필수 입니다")
+
     private int price;
-    @NotEmpty(message = "상품수량은 필수 입니다")
+
     private int stockQuantity;
 
     //책과 관려된 특별한 속성
     @NotEmpty(message = "책 저자는 필수 입니다")
     private String author;
+
     @NotEmpty(message = "ISBN 은 필수 입니다")
     private String isbn;
 }
